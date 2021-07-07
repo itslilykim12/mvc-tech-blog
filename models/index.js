10ff27c55f08b9aa009677c10513e8ccd0113f5a
nlilu <nlilum.kim@gmail.com>
2021-07-06 20:34:13 -0400

    created index, post and comment models

--- models/index.js
+++ models/index.js
@@ -1,8 +1,31 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
 
 //User-Post relationship 
 User.hasMany(Post, {
     foreignKey: "user_id"
 });
+//Post-User relationship
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+});
+//Comment-User relationship
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+});
+//Comment-Post relationship
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+});
+//User-Comment relationship
+User.hasMany(Comment, {
+    foreignKey: "user_id"
+});
+//Post-Comment relationship
+Post.hasMany(Comment, {
+    foreignKey: "post_id"
+});
 
-module.exports = { User };
+module.exports = { User, Post, Comment };
